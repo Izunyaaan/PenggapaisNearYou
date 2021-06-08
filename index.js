274d2b9a76d1defe44ad36bb0bbf6b3e51df5c24
Izunyaaan <kanebluerivers@gmail.com>
2021-06-08 21:33:52 +0800

    fixes

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 //declare port
 const PORT = process.env.PORT || 3000;
 
+//declare path
+const path = require('path');
+
 //set views folder and view engine
 app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
@@ -13,7 +16,7 @@ app.use(express.static('public'));
 
 //use urlencoder for parsing request body
 app.use(express.urlencoded({
-    extended=false
+    extended: false
 }));
 
 //database shit
