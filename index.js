5659aa813c17057e6738cdbca181db684f12a594
Izunyaaan <kanebluerivers@gmail.com>
2021-06-10 23:57:22 +0800

    another heroku pish

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 //declare port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 //declare path
 const path = require('path');
--- routes/app.js
+++ routes/app.js
@@ -5,5 +5,9 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.post('/search', (req, res) => {
+    res.render('searchResults');
+});
+
 
 module.exports = router;
